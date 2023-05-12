91a695953b7b968ddb761c3f12a26960bbe08075
simononze@naver.com <simononze@naver.com>
2023-05-13 01:25:09 +0900

    [proxy] 프록시를 적요할 3가지 버전 예시 프로젝트 생성 v1 : 수동등록 : 인터페이스 있을경우 v2 : 수동등록 : 인터페이스 없을경우 v3 : 자동등록

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,10 +1,17 @@
 package hello.proxy;
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+/**
+ * @Component 스캔 시작 패키지 대상을 hello.proxy.app 으로 함 -> config 패키지에 있는 @Configuration 은 컴포턴스 스캔 되지 않음
+ * @Configuration 이 자동 등록 되지 안으니 이렇게 @Import 를 사용해 수동으로 등록
+ */
+@SpringBootApplication(scanBasePackages = "hello.proxy.app")
+@Import({AppV1Config.class, AppV2Config.class})
 public class ProxyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
